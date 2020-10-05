95b320cd760ef25dec51c5b7aee4e7a87fc11a6f
Cole Schultz <schultzcole@gmail.com>
2020-10-04 20:19:18 -0500

    Update to match 0.7.3

--- backgroundless-pins.js
+++ backgroundless-pins.js
@@ -1,14 +1,31 @@
-import { BackgroundlessControlIcon } from "./backgroundless-control-icon.js";
-
-
 Hooks.on("init", () => {
     // Override map notes to use the BackgroundlessControlIcon
-    // the content of this method is copied from foundry.js:35629 (foundry version 0.6.2)
-    Note.prototype._drawControlIcon = function() {
+    Note.prototype._drawControlIcon = function () {
         let tint = this.data.iconTint ? colorStringToHex(this.data.iconTint) : null;
-        let icon = new BackgroundlessControlIcon({texture: this.data.icon, size: this.size, tint: tint});
-        icon.x -= (this.size / 2);
-        icon.y -= (this.size / 2);
+        let icon = new BackgroundlessControlIcon({ texture: this.data.icon, size: this.size, tint: tint });
+        icon.x -= this.size / 2;
+        icon.y -= this.size / 2;
         return icon;
-    }
+    };
 });
+
+export class BackgroundlessControlIcon extends ControlIcon {
+    /**
+     * Override ControlIcon#draw to remove drawing of the background.
+     */
+    async draw() {
+        // Draw border
+        this.border
+            .clear()
+            .lineStyle(2, this.borderColor, 1.0)
+            .drawRoundedRect(...this.rect, 5)
+            .endFill();
+        this.border.visible = false;
+
+        // Draw icon
+        this.icon.texture = this.texture ?? (await loadTexture(this.iconSrc));
+        this.icon.width = this.icon.height = this.size;
+        this.icon.tint = Number.isNumeric(this.tintColor) ? this.tintColor : 0xffffff;
+        return this;
+    }
+}
