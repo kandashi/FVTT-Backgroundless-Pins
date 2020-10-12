62d21031d6b32ff31fc068962274561802b3ebb1
Cole Schultz <schultzcole@gmail.com>
2020-10-12 17:46:59 -0500

    Allow toggling of background or not
    Resolves #1

--- backgroundless-pins.js
+++ backgroundless-pins.js
@@ -2,11 +2,29 @@ Hooks.on("init", () => {
     // Override map notes to use the BackgroundlessControlIcon
     Note.prototype._drawControlIcon = function () {
         let tint = this.data.iconTint ? colorStringToHex(this.data.iconTint) : null;
-        let icon = new BackgroundlessControlIcon({ texture: this.data.icon, size: this.size, tint: tint });
+        let iconData = { texture: this.data.icon, size: this.size, tint: tint };
+        let icon;
+        if (this.getFlag("backgroundless-pins", "hasBackground")) {
+            icon = new ControlIcon(iconData);
+        } else {
+            icon = new BackgroundlessControlIcon(iconData);
+        }
         icon.x -= this.size / 2;
         icon.y -= this.size / 2;
         return icon;
     };
+});
+
+Hooks.on("renderNoteConfig", (noteConfig, html, _) => {
+    const hasBackground = noteConfig.object.getFlag("backgroundless-pins", "hasBackground") ?? false;
+    const iconTintGroup = html.find("[name=iconTint]").closest(".form-group");
+    iconTintGroup.after(`
+        <div class="form-group">
+            <label for="flags.backgroundless-pins.hasBackground">Show Background?</label>
+            <input type="checkbox" name="flags.backgroundless-pins.hasBackground" data-dtype="Boolean" ${hasBackground ? "checked" : ""}>
+        </div>
+    `);
+    noteConfig.setPosition({ height: "auto" });
 });
 
 export class BackgroundlessControlIcon extends ControlIcon {
